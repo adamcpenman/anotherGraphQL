3348c30d6c98a242accdbee74869f6b1f62f6083
Adam <adamcpenman@gmail.com>
2020-03-30 15:39:46 -0500

    get stories

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import StoriesContainer from './containers/StoriesContainer';
 
 const App = () => {
   return (
-    <div className='App'>
-      <h1>Hello</h1>
+    <div>
+      <StoriesContainer />
     </div>
   );
 };
---
+++ src/containers/StoriesContainer.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import { getStoryIds } from '../services/hnApi';
+
+const StoriesContainer = () => {
+  const [storyIds, setStoryIds] = useState([]);
+
+  useEffect(() => {
+    getStoryIds().then(data => setStoryIds(data));
+  }, []);
+
+  return <p>{JSON.stringify(storyIds)}</p>;
+};
+
+export default StoriesContainer;
